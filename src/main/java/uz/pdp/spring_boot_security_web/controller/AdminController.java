2906a935fcce2858858ef4e296261c487755f45f
Bekzod <bekzodsadriddinov92@gmail.com>
2023-02-22 17:57:47 +0500

    Add tugadi

---
+++ src/main/java/uz/pdp/spring_boot_security_web/controller/AdminController.java
@@ -0,0 +1,23 @@
+package uz.pdp.spring_boot_security_web.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import uz.pdp.spring_boot_security_web.model.dto.receive.UserRegisterDTO;
+import uz.pdp.spring_boot_security_web.service.UserService;
+
+@Controller
+@RequestMapping("/api/admin")
+@RequiredArgsConstructor
+public class AdminController {
+    private final UserService userService;
+
+    @PostMapping("/add")
+    public String addUser(
+            @ModelAttribute UserRegisterDTO userRegisterDTO
+    ) {
+
+       userService.addAdmin(userRegisterDTO);
+        return "CrudAdmin";
+    }
+}
