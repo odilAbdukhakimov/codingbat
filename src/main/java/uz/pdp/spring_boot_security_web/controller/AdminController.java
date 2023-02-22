ff98e4f20b658c36266f5645a7eac9da0d9fe809
Axror <ahror060799@gmail.com>
2023-02-22 16:50:30 +0500

    AdminController qoshdim

---
+++ src/main/java/uz/pdp/spring_boot_security_web/controller/AdminController.java
@@ -0,0 +1,12 @@
+package uz.pdp.spring_boot_security_web.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("")
+@RequiredArgsConstructor
+public class AdminController {
+
+}
