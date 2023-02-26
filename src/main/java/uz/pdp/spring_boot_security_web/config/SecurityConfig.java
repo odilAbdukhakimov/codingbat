a27fc2208c55f606db2f824bdc6f9c242f0ec112
shodiyorbektolqinov7777 <shodiyorbektoqinov7777@gmail.vom>
2023-02-26 15:52:35 +0500

    redy all registration

--- src/main/java/uz/pdp/spring_boot_security_web/config/SecurityConfig.java
+++ src/main/java/uz/pdp/spring_boot_security_web/config/SecurityConfig.java
@@ -5,6 +5,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
@@ -16,6 +18,8 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import uz.pdp.spring_boot_security_web.service.AuthService;
+
+import java.util.Properties;
 
 @Configuration
 @EnableMethodSecurity
@@ -50,6 +54,7 @@ public class SecurityConfig {
                 .requestMatchers(HttpMethod.POST, "/register").permitAll()
                 .requestMatchers(HttpMethod.POST, "/api/user/add").permitAll()
                 .requestMatchers(HttpMethod.POST, "/api/admin/add").permitAll()
+                .requestMatchers(HttpMethod.POST, "/api/admin/list").permitAll()
 //                .requestMatchers(HttpMethod.DELETE, "/admin/lang/delete/*").permitAll()
                 .anyRequest()
                 .authenticated()
@@ -59,10 +64,27 @@ public class SecurityConfig {
                 .and()
                 .logout()
                 .logoutUrl("/logout")
+                .logoutSuccessUrl("/")
                 .deleteCookies("JSESSIONID");
 
         return http.build();
     }
+    @Bean
+    public JavaMailSender javaMailSender(){
+        JavaMailSenderImpl javaMailSender=new JavaMailSenderImpl();
+        javaMailSender.setHost("smtp.gmail.com");
+        javaMailSender.setPort(587);
+        javaMailSender.setUsername("shodiyorbektolqinov7777@gmail.com");
+        javaMailSender.setPassword("shodiyorbek2007");
+        Properties properties=javaMailSender.getJavaMailProperties();
+        properties.put("mail.transport.protocol","smtp");
+        properties.put("mail.smtp.auth","true");
+        properties.put("mail.smtp.starttls.enable","true");
+        properties.put("mail.debug","true");
+        return javaMailSender;
+
+    }
+
 
     @Bean
     public AuthenticationProvider authenticationProvider() {
