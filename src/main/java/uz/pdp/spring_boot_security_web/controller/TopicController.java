716b85fa2bdb6cc4d1d60602bf3e11722961795f
Sayidalokhon <sayidalokhonkham@gmail.com>
2023-02-26 16:24:07 +0500

    Task page

--- src/main/java/uz/pdp/spring_boot_security_web/controller/TopicController.java
+++ src/main/java/uz/pdp/spring_boot_security_web/controller/TopicController.java
@@ -4,11 +4,11 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+import uz.pdp.spring_boot_security_web.entity.LanguageEntity;
 import uz.pdp.spring_boot_security_web.entity.UserEntity;
+import uz.pdp.spring_boot_security_web.model.dto.TopicRequestDTO;
 import uz.pdp.spring_boot_security_web.service.AuthService;
 import uz.pdp.spring_boot_security_web.service.LanguageService;
 import uz.pdp.spring_boot_security_web.service.TaskService;
@@ -21,20 +21,59 @@ public class TopicController {
     private final LanguageService languageService;
     private final TaskService taskService;
     private final AuthService authService;
+    private final TopicService topicService;
 
     @GetMapping("/{language}/{id}")
     public ModelAndView getTaskList(
             ModelAndView modelAndView,
             @PathVariable String language,
-            @PathVariable int id
+            @PathVariable String topic
     ) {
         UserEntity user = authService.findUserByUsername();
         modelAndView.addObject("subjectList", languageService.languageEntityList());
-        modelAndView.addObject("taskList", taskService.getTaskList(id));
+//        modelAndView.addObject("taskList", taskService.getTaskList(id));
         if (user != null){
             modelAndView.addObject("userTasksList", taskService.userTaskEntityList(String.valueOf(user.getUsername())));
         }
         modelAndView.setViewName("task");
+        modelAndView.addObject("taskList", taskService.getTaskListByTopicAndLanguage(
+                language, topic
+        ));
+        modelAndView.setViewName("question");
         return modelAndView;
+    }
+
+    @GetMapping("admin/{language}/{id}")
+        public ModelAndView topicHome( @PathVariable int id, ModelAndView model){
+        LanguageEntity byId = languageService.getById(id);
+        model.addObject("language", byId);
+        model.addObject("topicList", byId.getTopicEntities());
+        model.setViewName("topic");
+        return model;
+    }
+
+    @PostMapping("/admin/topic/add")
+    public String addTopic(
+            @ModelAttribute TopicRequestDTO topicRequestDTO
+    ) {
+        topicService.add(topicRequestDTO);
+        return "redirect:/admin/lang";
+    }
+
+    @GetMapping("/admin/topic/del/{id}")
+    public String deleteTopic(
+            @PathVariable int id
+    ) {
+        topicService.delete(id);
+        return "redirect:/admin/lang";
+    }
+
+    @PostMapping("/admin/topic/update/{id}")
+    public String updateTopic(
+            @PathVariable int id,
+            @ModelAttribute TopicRequestDTO topicRequestDTO
+    ) {
+        topicService.update(id, topicRequestDTO);
+        return "redirect:/admin/lang";
     }
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,20 +1,4 @@
-create table if not exists role_cb(
-    id serial primary key,
-    name varchar not null
-);
-create table if not exists permission_cb(
-    id serial primary key,
-    name varchar not null
-);
-
-insert into role_cb(name) values ('ADMIN');
-insert into role_cb(name) values ('USER');
-insert into role_cb(name) values ('SUPER_ADMIN');
-
-insert into permission_cb(name) values ('ADD');
-insert into permission_cb(name) values ('DELETE');
-insert into permission_cb(name) values ('EDIT');
-insert into permission_cb(name) values ('READ');
-
+insert into language_entity(title) values ('Java');
+insert into language_entity(title) values ('Python');
 
 
